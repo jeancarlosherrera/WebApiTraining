73e1a632c1b1fbecfbac08beb889a63c57d1706e
Jean C. Herrera <jeancarlosherreram@gmail.com>
2024-05-08 16:21:25 -0400

    Last day

--- src/IssueTrackerSolution/IssueTracker.Api/Catalog/Models.cs
+++ src/IssueTrackerSolution/IssueTracker.Api/Catalog/Models.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Riok.Mapperly.Abstractions;
 
 namespace IssueTracker.Api.Catalog;
 
@@ -7,12 +8,55 @@ namespace IssueTracker.Api.Catalog;
 
 public record CreateCatalogItemRequest(string Title, string Description);
 
+public static class ModelExtensions
+{
+    //public static CatalogItemResponse MapToResponse(this CatalogItem item)
+    //{
+    //    return new CatalogItemResponse(item.Id, item.Title, item.Description);
+    //}
+    public static CatalogItem MapToCatalogItem(this CreateCatalogItemRequest request, string addedBy)
+    {
+        return new CatalogItem
+        {
+            Id = Guid.NewGuid(),
+            Description = request.Description,
+            Title = request.Title,
+            AddedBy = addedBy,
+            CreatedAt = DateTimeOffset.Now
+        };
+    }
+}
+
+
+[Mapper]
+public static partial class CatalogMappers
+{
+    public static partial CatalogItemResponse MapToResponse(this CatalogItem item);
+}
+
+
 public class CreateCatalogItemRequestValidator : AbstractValidator<CreateCatalogItemRequest>
 {
     public CreateCatalogItemRequestValidator()
     {
-        RuleFor(r => r.Title).NotEmpty().MinimumLength(5).MaximumLength(256);
+        RuleFor(r => r.Title).NotEmpty().WithMessage("We Need A Title");
+
+        RuleFor(r => r.Title).MinimumLength(5).MaximumLength(256).WithMessage("Has to be between 5 and 256 characters");
         RuleFor(r => r.Description).NotEmpty().MaximumLength(1024);
     }
 }
 public record CatalogItemResponse(Guid Id, string Title, string Description);
+
+public record ReplaceCatalogItemRequest(Guid Id, string Title, string Description);
+
+public class CatalogItemResponseValidator : AbstractValidator<ReplaceCatalogItemRequest>
+{
+    public CatalogItemResponseValidator()
+    {
+        RuleFor(r => r.Id).NotEmpty(); // todo add to check if it is a guid
+        RuleFor(r => r.Title).NotEmpty().WithMessage("We Need A Title");
+
+        RuleFor(r => r.Title).MinimumLength(5).MaximumLength(256).WithMessage("Has to be between 5 and 256 characters");
+        RuleFor(r => r.Description).NotEmpty().MaximumLength(1024);
+    }
+}
--- src/IssueTrackerSolution/IssueTracker.Api/Program.cs
+++ src/IssueTrackerSolution/IssueTracker.Api/Program.cs
@@ -1,18 +1,30 @@
 using FluentValidation;
 using IssueTracker.Api.Catalog;
+using IssueTracker.Api.Shared;
 using Marten;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+// sets up the auth stuff to read from our environment specific config.
 builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddScoped<IAuthorizationHandler, ShouldBeCreatorOfCatalogItemRequirementHandler>();
 
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<UserIdentityService>();
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("IsSoftwareAdmin", policy =>
     {
         policy.RequireRole("SoftwareCenter");
-
+        policy.AddRequirements(new ShouldBeCreatorToAlterCatalogItemRequirement());
     });
 });
 
@@ -21,10 +33,43 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddFluentValidationRulesToSwagger();
+builder.Services.AddSwaggerGen(options =>
+{
+
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header with bearer token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                },
+                Scheme = "oauth2",
+                Name = "Bearer ",
+                In = ParameterLocation.Header
+            },[]
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    options.DocInclusionPredicate((_, api) => !string.IsNullOrWhiteSpace(api.GroupName));
+    options.EnableAnnotations();
+}); // this will add the stuff to generate an OpenApi specification.
 //builder.Services.AddSingleton<IValidator<CreateCatalogItemRequest>, CreateCatalogItemRequestValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<CreateCatalogItemRequestValidator>();
 
